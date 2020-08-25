11cabb97ce0fd94866ba4d0565de9e51233f928b
Dhanis AL Ghifari <dhanisalghifari@gmail.com>
2020-08-26 01:13:18 +0700

    create delete section todos

--- src/components/Todo/Todo.js
+++ src/components/Todo/Todo.js
@@ -1,28 +1,35 @@
-import React, { useState } from 'react'
-import TodoForm from '../TodoForm/TodoForm'
-import TodoList from '../TodoList/TodoList'
-import { RiDeleteBin2Line } from 'react-icons/ri'
-import { TiEdit } from 'react-icons/ti'
+import React, { useState } from "react";
+import TodoForm from "../TodoForm/TodoForm";
+import TodoList from "../TodoList/TodoList";
+import { RiDeleteBin2Line } from "react-icons/ri";
+import { TiEdit } from "react-icons/ti";
 
-function Todo({todos, completeTodo}) {
+function Todo({ todos, completeTodo, removeTodo }) {
+  const [edit, setEdit] = useState({
+    id: null,
+    value: "",
+  });
 
-    const [edit, setEdit] = useState({
-        id: null,
-        value: ''
-    })
-
-    return todos.map((todo, index) => (
-        <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
-            <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-                {todo.text}
-            </div>
-            <div className="icons">
-                <RiDeleteBin2Line />
-                <TiEdit />
-            </div>
-
-        </div>
-    ))
+  return todos.map((todo, index) => (
+    <div
+      className={todo.isComplete ? "todo-row complete" : "todo-row"}
+      key={index}
+    >
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons">
+        <RiDeleteBin2Line
+          onClick={() => removeTodo(todo.id)}
+          className="delete-icon"
+        />
+        <TiEdit
+          onClick={() => setEdit({ id: todo.id, value: todo.text })}
+          className="delete-icon"
+        />
+      </div>
+    </div>
+  ));
 }
 
-export default Todo
+export default Todo;
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,36 +1,40 @@
-import React, { useState } from 'react'
-import TodoForm from '../TodoForm/TodoForm'
-import Todo from '../Todo/Todo';
-
+import React, { useState } from "react";
+import TodoForm from "../TodoForm/TodoForm";
+import Todo from "../Todo/Todo";
 
 function TodoList() {
-    const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = todo => {
-        if(!todo.text || /^\s*$/.test(todo.text)) {
-            return;
-        }
-        const newTodos = [todo, ...todos];
-        setTodos(newTodos);
-    };
-
-    const completeTodo = id => {
-        let updatedTodos = todos.map(todo => {
-            if (todo.id === id) {
-                todo.isComplete = !todo.isComplete
-            }
-            return todo;
-        })
-        setTodos(updatedTodos);
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
     }
+    const newTodos = [todo, ...todos];
+    setTodos(newTodos);
+  };
 
-    return (
-        <div>
-            <h1>What's your plan today?</h1>
-            <TodoForm onSubmit={addTodo} />
-            <Todo todos={todos} completeTodo={completeTodo} />
-        </div>
-    )
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    setTodos(removeArr);
+  };
+
+  return (
+    <div>
+      <h1>What's your plan today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} />
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
